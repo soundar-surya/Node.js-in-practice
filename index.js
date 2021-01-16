701d3db8342b8ee59a4a68f48d95a2634326ed68
surya <bittergourd76@gmail.com>
2021-01-16 15:44:31 +0530

    configured server

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const http = require('http');
 const {aim, amz} = require('./scrape/app');
 
-console.log(aim, amz)   //functions
+
+const server = http.createServer( (req, res) => {
+        switch(req.url){
+            case '/home':
+                let body = [];
+                req.on('data',  chunk => body.push(chunk))
+                    .on('end', () => {
+                        body = Buffer.concat(body).toString()
+                        console.log(JSON.parse(body))
+                    })
+                    .on('err', e => console.log(e))
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Hello world');
+                break;
+
+            default:
+                res.statusCode = 404;
+                res.setHeader('Content-type', 'text/plain');
+                res.end('Oops, Page Not Found');
+        }
+    } );
+
+
+const PORT = 8000 || process.env.PORT;
+
+server.listen(PORT, () =>
+    console.log(`listening on port ${PORT}`)
+);
+
+//console.log(aim, amz)   //functions
