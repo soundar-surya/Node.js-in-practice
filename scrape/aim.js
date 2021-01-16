2fd18ec8f419084a3361b317282b584497208a54
surya <bittergourd76@gmail.com>
2021-01-16 18:10:03 +0530

    updated env

--- scrape/aim.js
+++ scrape/aim.js
@@ -1,9 +1,9 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-module.exports =  async () =>  {
-        const browser = await puppeteer.launch({headless: true});
-        const page = await browser.newPage();
-        await page.goto('https://analyticsindiamag.com/');
-        await page.screenshot({path: 'output.png'});
-        await browser.close();
-    }
+// module.exports =  async () =>  {
+//         const browser = await puppeteer.launch({headless: true});
+//         const page = await browser.newPage();
+//         await page.goto('https://analyticsindiamag.com/');
+//         await page.screenshot({path: 'output.png'});
+//         await browser.close();
+//     }
