f185447ff9be9359d7df706d2cca35d3a12b3226
surya <bittergourd76@gmail.com>
2021-01-16 16:47:51 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer( (req, res) => {
         switch(req.url){
             case '/home':
                 let body = [];
-                req.on('data',  chunk => body.push(chunk))
+                 req.on('data',  chunk => body.push(chunk))
                     .on('end', () => {
                         body = Buffer.concat(body).toString()
                         console.log(JSON.parse(body))
