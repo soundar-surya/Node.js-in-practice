02cc7a7fe066dc9125f5893032c2b12f868d4b99
surya <bittergourd76@gmail.com>
2021-01-16 17:50:50 +0530

    updated env vars

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "49d01c161cmsh8b9b08e1de1651bp1987f6jsnd6f25bf779e2",
+	host: "amazon-product-reviews-keywords.p.rapidapi.com",
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: process.env.key,
+	host: process.env.host,
+}
