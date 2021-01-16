d0eba912342ce0b9aaf95bf28e789644ed19bb43
surya <bittergourd76@gmail.com>
2021-01-16 19:33:01 +0530

    updated routes

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const http = require('http');
 const {aim, amz} = require('./scrape/main');
 
-
 const server = http.createServer( (req, res) => {
-        switch(req.url){
-            case '/home':
-                let body = [];
-                 req.on('data',  chunk => body.push(chunk))
-                    .on('end', () => {
-                        body = Buffer.concat(body).toString()
-                        console.log(JSON.parse(body))
-                        
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'text/plain');
-                        res.end('Hello there!');
-                    })
-                    .on('err', e => console.log(e))
-          
-                break;
-
-            default:
-                res.statusCode = 404;
-                res.setHeader('Content-type', 'text/plain');
-                res.end('Oops, Page Not Found');
+        let {url, method} = req;
+        if(url === '/home' && method === 'POST'){
+            let body = [];
+            
+                req.on('data', chunk => body.push(chunk))
+                .on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    console.log(JSON.parse(body))
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end(body);
+                })
+                .on('error', e => console.log(e))
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-type', 'text/plain');
+            res.end('Page Not Found');
         }
     } );
 
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT ||  8000;
 
 server.listen(PORT, () =>
     console.log(`listening on port ${PORT}`)
